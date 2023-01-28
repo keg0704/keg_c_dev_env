df0c8a0f67f64f35aafe627087dcbefd6764e2c8
keg0704 <91190770+keg0704@users.noreply.github.com>
2023-01-28 20:09:07 +0800

    set relativenumber to true in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -113,6 +113,9 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Set relative numbers to true
+vim.o.relativenumber = true
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
